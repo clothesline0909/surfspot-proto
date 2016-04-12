09f9398380e38d0f55a02d88dc92aa0935bddeca
Brett Clark <BClark@3DCMELB.COM.AU>
2016-04-12 17:45:46 +1000

    thinking about back-end API

--- app/backend/app.run.mock.js
+++ app/backend/app.run.mock.js
@@ -5,4 +5,12 @@ angular.module('SurfSpot')
     $httpBackend.whenGET(/templates\/.*/).passThrough();
     $httpBackend.whenGET(/directives\/.*/).passThrough();
 
+    /* HTTP GET requests:
+        // Returns current information about a particular spot.
+        /api/spots/current/@id
+        // Returns current information about the best matching spots.
+        /api/spots/search?experience=35&board-length=2.286&...
+        // Returns average information about a spot by month
+        /api/spots/@id/average
+
 }]);
