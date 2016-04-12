71a9ccbf1d8df4b65ec3c609e78285c45b7d52ea
Brett Clark <BClark@3DCMELB.COM.AU>
2016-04-12 15:43:03 +1000

    ssSlider movement connected to scope

--- app/directives/ssSlider.js
+++ app/directives/ssSlider.js
@@ -7,52 +7,56 @@ angular.module('SurfSpot')
         },
         templateUrl: 'templates/directives/ssSlider.html',
         replace: true,
-        link: function(scope, elem, attrs) {
+        link: function(scope, slider, attrs) {
             scope.clicked = false;
-            renderSlider(elem, scope.position);
 
-            var tab = elem.find('.slider-tab');
+            scope.renderSlider = function () {
+                var tab = slider.find('.slider-tab');
+                var tabWidth = tab.width();
+                var parentWidth = slider.width();
+                var newLeft = (parentWidth - tabWidth) * scope.position / 100;
+                tab.css('left', newLeft + "px");
+            }
+
+            scope.getNewPosition = function (distance) {
+                var tab = slider.find('.slider-tab');
+                var oldLeft = Number(tab.css('left').replace('px', ''));
+                var tabWidth = tab.width();
+                var parentWidth = slider.width();
+                var positionDif = distance / (parentWidth - tabWidth) * 100;
+                var newPosition = scope.position + positionDif;
+                if (newPosition < 0 || newPosition > 100) {
+                    newPosition = scope.position;
+                }
+                return newPosition;
+            }
+
+            scope.renderSlider();
+
+            var tab = slider.find('.slider-tab');
 
             var startPointX;
 
             tab.bind('mousedown', function (event) {
                 scope.clicked = true;
                 startPointX = event.clientX;
+                scope.$digest();
             });
 
             jQuery(document).bind('mouseup', function () {
                 scope.clicked = false;
+                scope.$digest();
             });
 
-            tab.bind('mousemove', function (event) {
-                if (clicked) {
+            jQuery(document).bind('mousemove', function (event) {
+                if (scope.clicked) {
                     var distanceX = event.clientX - startPointX;
-                    scope.position = getNewPosition(elem, scope.position, distanceX);
-                    renderSlider(elem, scope.position);
+                    scope.position = scope.getNewPosition(distanceX);
+                    scope.renderSlider();
                     startPointX = event.clientX;
+                    scope.$digest();
                 }
             });
         }
     }
 });
-
-function renderSlider(slider, position) {
-    var tab = slider.find('.slider-tab');
-    var tabWidth = tab.width();
-    var parentWidth = slider.width();
-    var newLeft = (parentWidth - tabWidth) * position / 100;
-    tab.css('left', newLeft + "px");
-}
-
-function getNewPosition(slider, position, distance) {
-    var tab = slider.find('.slider-tab');
-    var oldLeft = Number(tab.css('left').replace('px', ''));
-    var tabWidth = tab.width();
-    var parentWidth = slider.width();
-    var positionDif = distance / (parentWidth - tabWidth) * 100;
-    var newPosition = position + positionDif;
-    if (newPosition < 0 || newPosition > 100) {
-        newPosition = position;
-    }
-    return position;
-}
--- spec/directives/ssSliderSpec.js
+++ spec/directives/ssSliderSpec.js
@@ -7,61 +7,113 @@ describe('SsSlider', function () {
     var $compile,
         $rootScope,
         scope,
-        element;
+        slider,
+        sliderWidth,
+        tab,
+        tabWidth,
+        left;
 
     beforeEach(inject(function (_$compile_, _$rootScope_) {
         $compile = _$compile_;
         $rootScope = _$rootScope_;
         scope = $rootScope.$new();
-        element = create();
+        slider = create();
+        angular.element(document).find('body').append(slider);
+        slider.isolateScope().renderSlider();
+        sliderWidth = slider.width();
+        tab = slider.find('.slider-tab');
+        tabWidth = tab.width();
+        left = Number(tab.css('left').replace('px', ''));
     }));
 
+    afterEach(function () {
+        slider.remove();
+    });
+
     function create() {
-        var element, compiledElement;
-        element = angular.element('<ss-slider position="40"></ss-slider>');
-        compiledElement = $compile(element)(scope);
+        var slider, compiledslider;
+        slider = angular.element('<ss-slider position="40"></ss-slider>');
+        compiledslider = $compile(slider)(scope);
         scope.$digest();
-        return compiledElement;
+        return compiledslider;
     }
 
-    it("should replace the element with the appropriate content", function () {
-        expect(element.html()).toContain('<div class="slider-bar">');
+    describe(".renderSlider", function () {
+        it("should render the slider tab at 40% of the length", function () {
+            var percentage = left / (sliderWidth - tabWidth) * 100;
+            expect(percentage).toBe(40);
+        });
+    });
+
+    describe(".getNewPosition", function () {
+
+        it("should give new position as 50", function () {
+            var distanceX = 0.1 * (sliderWidth - tabWidth);
+            expect(slider.isolateScope().getNewPosition(distanceX)).toBe(50);
+        });
+
+        it("should give new position as 30", function () {
+            var distanceX = -0.1 * (sliderWidth - tabWidth);
+            expect(slider.isolateScope().getNewPosition(distanceX)).toBe(30);
+        });
+
+        it("should give new position as 40", function () {
+            var distanceX = 0.7 * (sliderWidth - tabWidth);
+            expect(slider.isolateScope().getNewPosition(distanceX)).toBe(40);
+        });
+
+        it("should give new position as 40", function () {
+            var distanceX = -0.5 * (sliderWidth - tabWidth);
+            expect(slider.isolateScope().getNewPosition(distanceX)).toBe(40);
+        });
+    });
+
+    it("should replace the slider with the appropriate content", function () {
+        expect(slider.html()).toContain('<div class="slider-bar">');
     });
 
     it("should set the position to 40", function () {
-        expect(element.isolateScope().position).toBe(40);
-    });
-
-    it("should render the slider tab at 40% of the length", function () {
-        angular.element(document).find('body').append(element);
-        var tab = element.find('.slider-tab');
-        var left = Number(tab.css('left').replace('px', ''));
-        var parentWidth = element.width();
-        var tabWidth = tab.width();
-        var percentage = left / (parentWidth - tabWidth) * 100;
-        expect(percentage).toBe(40);
+        expect(slider.isolateScope().position).toBe(40);
     });
 
     it("should begin as un-clicked", function () {
-        expect(element.isolateScope().clicked).toBe(false);
+        expect(slider.isolateScope().clicked).toBe(false);
     });
 
     it("should be clicked when a mousedown event occurs on the slider tab", function () {
-        var tab = element.find('.slider-tab');
+        var tab = slider.find('.slider-tab');
         tab.trigger('mousedown');
-        expect(element.isolateScope().clicked).toBe(true);
+        expect(slider.isolateScope().clicked).toBe(true);
     });
 
     it("should be not clicked when a mouseup event occurs on the document", function () {
-        element.isolateScope().clicked = true;
-        expect(element.isolateScope().clicked).toBe(true);
-        var tab = element.find('.slider-tab');
+        slider.isolateScope().clicked = true;
+        expect(slider.isolateScope().clicked).toBe(true);
         $(document).trigger('mouseup');
-        expect(element.isolateScope().clicked).toBe(false);
+        expect(slider.isolateScope().clicked).toBe(false);
     });
 
-    it("should modify 'position' in response to user dragging slider", function () {
-        pending('');
+    it("should give position 40 when mouse is dragged without click", function () {
+        var event = $.Event('mousemove');
+        event.clientX = 0.3 * (sliderWidth - tabWidth);
+        tab.trigger(event);
+        expect(slider.isolateScope().position).toBe(40);
+    });
+
+    it("should give position 70 when mouse is dragged with click", function () {
+        var event = $.Event('mousedown');
+        startX = tab.offset().left + tab.width() / 2;
+        event.clientX = startX;
+        tab.trigger(event);
+        event = $.Event('mousemove');
+        event.clientX = startX + 0.3 * (sliderWidth - tabWidth);
+        tab.trigger(event);
+        expect(slider.isolateScope().position).toBe(70);
+    });
+
+    it("should have the class 'clicked' if clicked", function () {
+        tab.trigger('mousedown');
+        expect(tab.hasClass('clicked')).toBe(true);
     });
     
 });
